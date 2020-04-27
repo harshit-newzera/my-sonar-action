ce217d5ffc1cb19cb7538a7af25ca3f576d8613b
harshit-newzera <63589650+harshit-newzera@users.noreply.github.com>
2020-04-27 11:00:22 +0530

    add entrypoint

---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
+	EVENT_ACTION=$(jq -r ".action" "${GITHUB_EVENT_PATH}")
+	if [[ "${EVENT_ACTION}" != "opened" ]]; then
+		echo "No need to run analysis. It is already triggered by the push event."
+		exit 78
+	fi
+fi
+
+sonar-scanner \
+	-Dsonar.host.url=${INPUT_HOST} \
+	-Dsonar.projectKey=${PWD##*/} \
+	-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
+	-Dsonar.login=${INPUT_LOGIN} \
+	-Dsonar.password=${INPUT_PASSWORD} \
+	-Dsonar.sources=. \
+	-Dsonar.sourceEncoding=UTF-8
